676668a54dfd49c1cd96653cfefad5ccb6c6df10
Mohammed Farshad <imohammedfarshad@gmail.com>
2025-04-16 18:30:37 +0530

    chore: add light theme and colors

---
+++ lib/theme/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color primary = Color(0xFF6C63FF); 
+  static const Color secondary = Color(0xFF17B8A6); 
+  static const Color error = Color(0xFFF50057); 
+  static const Color success = Color(0xFF0FFF49);
+  static const Color background = Color(0xFFDADADA);
+}
---
+++ lib/theme/theme.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+import 'package:notes/theme/colors.dart';
+
+ThemeData lightTheme = ThemeData(
+  primaryColor: AppColors.primary,
+  scaffoldBackgroundColor: AppColors.background,
+  colorScheme: ColorScheme.light(
+    primary: AppColors.primary,
+    secondary: AppColors.secondary,
+    error: AppColors.error,
+  )
+);
